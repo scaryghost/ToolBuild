d5085856eb70a9ac680fa73ffd048e56281bd499
etsai <etsai2@gmail.com>
2014-03-16 22:01:49 -0700

    Fixed errors with the current setup
    Hooked into the rules with the CppUtilities project

--- common.mk
+++ common.mk
@@ -1,24 +1,35 @@
-MODULES_CONFIG:= $(addsuffix /src/config.mk, $(MODuLES))
-MODULES_RULES:= $(addsuffix /src/rules.mk, $(MODuLES))
+BUILD_DIR:=build
+DIST_DIR?=dist
+
+SRCS:=
+
+MODULES_CONFIG:= $(addsuffix /src/config.mk, $(MODULES))
+MODULES_RULES:= $(addsuffix /src/rules.mk, $(MODULES))
 
 include $(MODULES_CONFIG)
-include $(MODULES_RULES)
+-include $(MODULES_RULES)
 
 CXXFLAGS:=$(C_FLAGS) $(addprefix -I,$(INC_DIRS))
 ifeq ($(PLATFORM),x86)
     CXXFLAGS+=-m32
 else
-    ifeq($(PLATFORM),x64)
+    ifeq ($(PLATFORM),x64)
         CXXFLAGS+=-m64
     else
         $(error Unrecognized PLATFORM value, use x86 or x64)
     endif
 endif
-OJBS:=$(addprefix $(BUILD_DIR)/$(PLATFORM)/,$(SRCS:%.c=%.o))
+OBJS:=$(addprefix $(BUILD_DIR)/$(PLATFORM)/,$(SRCS:%.cpp=%.o))
 
 APP_OUTPUT:=$(DIST_DIR)/$(PLATFORM)/$(APP_NAME)
 
-all: $(APP_OUTPUT) $(DIST_DIR)/$(PLATFORM)
+all: $(BUILD_DIR)/$(PLATFORM) $(DIST_DIR)/$(PLATFORM) $(APP_OUTPUT)
+
+$(BUILD_DIR)/$(PLATFORM)/%.o: %.cpp
+	$(CXX) -c -o $@ $(CXXFLAGS) $<
+
+$(BUILD_DIR)/$(PLATFORM):
+	find $(MODULES) -type d -exec mkdir -p -- "$(BUILD_DIR)/$(PLATFORM)/{}" \;
 
 $(DIST_DIR)/$(PLATFORM):
 	mkdir -p $@
